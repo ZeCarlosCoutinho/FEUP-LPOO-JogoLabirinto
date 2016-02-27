41299c3773d523729bd8540fa19bfad6f6227926
Francisca96 <franciscapauperio@gmail.com>
2016-02-27 20:30:25 +0000

    commit1

---
+++ src/Dragao.java
@@ -0,0 +1,30 @@
+
+public class Dragao {
+	private int posx, posy;
+	private boolean morto;
+	
+	public int getPosx() {
+		return posx;
+	}
+	public void setPosx(int posx) {
+		this.posx = posx;
+	}
+	public int getPosy() {
+		return posy;
+	}
+	public void setPosy(int posy) {
+		this.posy = posy;
+	}
+	public boolean isMorto() {
+		return morto;
+	}
+	public void setMorto(boolean morto) {
+		this.morto = morto;
+	}
+	
+	public Dragao(){
+		posx=1;
+		posy=3;	
+		morto=false;
+	}
+}
