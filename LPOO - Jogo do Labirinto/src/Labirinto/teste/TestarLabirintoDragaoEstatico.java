e31854d5917165336928f97aa36ca769b4dbffa2
Francisca96 <franciscapauperio@gmail.com>
2016-03-03 17:39:52 +0000

    Ta a pintar!
    Teste 2a check
    Teste 2b check
    Vou tentando fazer e se necessitar de ajuda (vou necessitar certo) falo
    ;)

--- LPOO - Jogo do Labirinto/src/Labirinto/teste/TestarLabirintoDragaoEstatico.java
+++ LPOO - Jogo do Labirinto/src/Labirinto/teste/TestarLabirintoDragaoEstatico.java
@@ -5,10 +5,25 @@ import logic.*;
 
 public class TestarLabirintoDragaoEstatico {
 
-	@Test
-	public void test() {
+	//@Test
+	public void test2a() {
 		Labirinto lab = new Labirinto();
+		Heroi hero = new Heroi();
 		
+		lab.move_SerAnimado(hero, 1);
+		assertEquals(2, hero.getPosx());
+		assertEquals(1, hero.getPosy());	
 	}
+	
+	//@Test
+	public void test2b() {
+		Labirinto lab = new Labirinto();
+		Heroi hero = new Heroi();
+		
+		lab.move_SerAnimado(hero, 0);
+		assertEquals(1, hero.getPosx());
+		assertEquals(1, hero.getPosy());
+	}
+	
 
 }
