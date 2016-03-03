1862591e7a2faea126407dfa085cff207c3bcb36
Francisca96 <franciscapauperio@gmail.com>
2016-03-03 13:02:07 +0000

    2º round - Testes
    Começar Testes! Preciso de saber como se faz e gostava de fazer eu.

---
+++ LPOO - Jogo do Labirinto/src/Labirinto/teste/TestarLabirintoDragaoEstatico.java
@@ -0,0 +1,14 @@
+package Labirinto.teste;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import logic.*;
+
+public class TestarLabirintoDragaoEstatico {
+
+	@Test
+	public void test() {
+		Labirinto lab = new Labirinto();
+		
+	}
+
+}
